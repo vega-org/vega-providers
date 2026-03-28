12bc5cc49f67a368d9bf068eed3399d93f0920fe
touhidss <135936344+touhidss@users.noreply.github.com>
2026-03-28 11:03:19 +0530

    Update stream.ts
    new

--- providers/hiAnime/stream.ts
+++ providers/hiAnime/stream.ts
@@ -8,49 +8,43 @@ export const getStream = async function ({
   providerContext: ProviderContext;
 }): Promise<Stream[]> {
   try {
-    const { getBaseUrl, axios } = providerContext;
-    const baseUrl = await getBaseUrl("consumet");
-    const servers = ["vidcloud", "vidstreaming"];
-    const url = `${baseUrl}/anime/zoro/watch?episodeId=${id}&server=`;
-    const streamLinks: Stream[] = [];
-    await Promise.all(
-      servers.map(async (server) => {
-        try {
-          const res = await axios.get(url + server);
-          if (res.data) {
-            const subtitles: TextTracks = [];
-            res.data?.subtitles.forEach((sub: any) => {
-              if (sub?.lang === "Thumbnails") return;
-              subtitles.push({
-                language: sub?.lang?.slice(0, 2) || "Und",
-                uri: sub?.url,
-                title: sub?.lang || "Undefined",
-                type: sub?.url?.endsWith(".vtt")
-                  ? "text/vtt"
-                  : "application/x-subrip",
-              });
-            });
-            res.data?.sources.forEach((source: any) => {
-              streamLinks.push({
-                server: server,
-                link: source?.url,
-                type: source?.isM3U8 ? "m3u8" : "mp4",
-                headers: {
-                  Referer: "https://megacloud.club/",
-                  Origin: "https://megacloud.club",
-                },
-                subtitles: subtitles,
-              });
-            });
-          }
-        } catch (e) {
-          console.log(e);
-        }
-      })
+    const { axios } = providerContext;
+
+    // get episode servers
+    const serverRes = await axios.get(
+      `https://hianime.to/ajax/v2/episode/servers?episodeId=${id}`
     );
-    return streamLinks;
+
+    const servers =
+      serverRes?.data?.data?.sub?.map((s: any) => s.serverId) || [];
+
+    const streams: Stream[] = [];
+
+    for (const server of servers) {
+      try {
+        const linkRes = await axios.get(
+          `https://hianime.to/ajax/v2/episode/sources?id=${id}&server=${server}`
+        );
+
+        const data = linkRes?.data?.data;
+
+        if (!data?.link) continue;
+
+        streams.push({
+          server: server,
+          link: data.link,
+          type: "m3u8",
+          headers: {
+            Referer: "https://hianime.to/",
+            Origin: "https://hianime.to",
+          },
+          subtitles: [],
+        });
+      } catch (e) {}
+    }
+
+    return streams;
   } catch (err) {
-    console.error(err);
     return [];
   }
 };
