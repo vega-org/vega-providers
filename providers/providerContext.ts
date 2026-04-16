93d8acec5ed2b885929f306cbd1eaf35515d9411
Himanshu <sangwanhimanshu8443@gmail.com>
2026-04-16 20:04:22 +0530

    fix: remove Aes

--- providers/providerContext.ts
+++ providers/providerContext.ts
@@ -3,12 +3,11 @@ import { getBaseUrl } from "./getBaseUrl";
 import { headers } from "./headers";
 import * as cheerio from "cheerio";
 import { ProviderContext } from "./types";
-import Aes from "react-native-aes-crypto";
 
 export const providerContext: ProviderContext = {
   axios,
   getBaseUrl,
+  Aes: null,
   commonHeaders: headers,
-  Aes,
   cheerio,
 };
